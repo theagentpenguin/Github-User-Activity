956bc284f28a0844be2085b619bb28b6083ded2f
theagentpenguin <gunaseelanabcd@gmail.com>
2025-06-16 00:01:11 +0530

    Finishing off

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 A simple command line interface (CLI) to fetch the recent activity of a GitHub user and display it in the terminal.
 
+To run this project, clone the repository to your machine and follow the instructions given below:
+
+1] Once you download the repository, open terminal and navigate to the project folder
+2] Do npm install if required
+3] Run the file using node command with the username as argument.
+eg: node gitActivity.js theagentpenguin
+
+The result should be visible in the console now. Feel free to share your feedback and suggestions.
+
 https://roadmap.sh/projects/github-user-activity
--- gitActivity.js
+++ gitActivity.js
@@ -5,18 +5,14 @@ const args = process.argv.slice(2);
 const userName = args[0];
 const dataPath = "https://api.github.com/users/"+userName+"/events"
 const emptyArray = [];
-let apiData;
+
 
 console.log(chalk.yellow(dataPath));
 
 if(!fs.existsSync('apiData.json')){
         fs.writeFileSync('apiData.json',JSON.stringify(emptyArray,null,2),'utf8');
     }
-/*
-const apiData = fs.writeFileSync('apiData.json',fetch(dataPath),utf8);
-This approach doesn't work, have to use async
-console.log(chalk.yellow("Code executed!"));
-*/
+
 
 async function fetchAndDisplayData(){
         const apiData = await fetch(dataPath);
