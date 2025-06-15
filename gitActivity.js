2cd13295f55d23a1a029ebc159aa5ce43272d395
theagentpenguin <gunaseelanabcd@gmail.com>
2025-06-15 22:03:43 +0530

    fetched data from API successfully, now get to work on the display part

--- gitActivity.js
+++ gitActivity.js
@@ -5,6 +5,7 @@ const args = process.argv.slice(2);
 const userName = args[0];
 const dataPath = "https://api.github.com/users/"+userName+"/events"
 const emptyArray = [];
+let apiData;
 
 console.log(chalk.yellow(dataPath));
 
@@ -16,3 +17,16 @@ const apiData = fs.writeFileSync('apiData.json',fetch(dataPath),utf8);
 This approach doesn't work, have to use async
 console.log(chalk.yellow("Code executed!"));
 */
+
+async function fetchData(){
+        const apiData = await fetch(dataPath);
+        if(!apiData.ok){
+            console.log("Data not fetched from API");
+        }
+        const data = await apiData.json();
+        const dataToWrite = JSON.stringify(data,null,2);
+
+        fs.writeFileSync('apiData.json',dataToWrite,'utf8');
+}
+
+fetchData();
